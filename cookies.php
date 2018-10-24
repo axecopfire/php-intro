3633dfdc3652a36158c2c4bbb8b4d1f37d39e318
axecopfire <braun.sn11@gmail.com>
2018-10-24 15:23:07 -0400

    init commit

---
+++ cookies.php
@@ -0,0 +1,37 @@
+<?php
+
+$name = "SomeName";
+$value = 100;
+// Expires in a week
+$expiration = time() + (60*60*24*7);
+setcookie($name, $value, $expiration);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+<?php
+
+if(isset($_COOKIE["SomeName"])) {
+
+  $someOne = $_COOKIE["SomeName"];
+
+  echo $someOne;
+
+} else {
+  $someOne = "";
+}
+
+
+?>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>PHP Test</title>
+</head>
+<body>
+<h1>Here is a test as to whether or not PHP is served via App Engine</h1>
+
+</body>
+</html>
