785342955a6db8dce7ae4fef3ee70699c307079b
axecopfire <braun.sn11@gmail.com>
2018-10-24 15:44:58 -0400

    trying a git pull to app engine

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>PHP Testing</title>
+</head>
+<body>
 <?php echo
 "<h1>Here is a test as to whether or not PHP is served via App Engine</h1>";
 ?>
+<h2>This was done from a Git Pull</h2>
+
+</body>
+</html>
+
+
